fc496e183fe0310e311e5a1bae792e737c7e588f
Votre Nom <Vous@exemple.com>
2025-05-17 21:16:20 +0200

    feat: Redirect unauthenticated users to login
    Adds authentication check and redirects to the login page if the user is not authenticated. Retrieves the user ID and displays it.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div className="flex flex-col max-w-lg space-y-3">Ok lets go!</div>;
+import { FetchUser } from "./(user)/actions/FetchUser";
+import { redirect } from "next/navigation";
+
+
+export default async function Home() {
+  const userid = await FetchUser();
+
+  if (!userid) {
+    return redirect("/login");
+  }
+
+  return <div className="flex flex-col max-w-lg space-y-3">Ok lets go ${userid}</div>;
 }
